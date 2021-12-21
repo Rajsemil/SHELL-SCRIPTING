aff86d9fdb5d5b2b6a0f093726adf541427e5732
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2021-12-21 12:34:44 +0530

    Add files via upload

---
+++ 5 Ifstr.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Syntax:
+# 		if [condition] 
+# 		then
+#    		statement
+#		else
+#			statement
+# 		fi
+# condition equal
+string = "starprakash"
+if [[ string == "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+fi
+""" condition not equal
+string != "starprakash"
+if [[ string == "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+"""fi
+""" condition less than  equal
+string != "starprakash"
+if [[ string -= "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+"""fi
+""" condition greater than equal
+string != "starprakash"
+if [[ string += "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+"""fi
+""" condition 
+string != "starprakash"
+if [[ string >= "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+"""fi
+""" condition 
+string != "starprakash"
+if [[ string <= "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+"""fi
+""" condition 
+string != "starprakash"
+if [[ string \= "starprakash" ]]
+than
+	echo "condition is true"
+else
+	echo "condition is false"
+"""fi
