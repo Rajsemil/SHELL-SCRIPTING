6cf32dd8abb0fc828bd907a70f5bc87c3b6f7dea
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2021-12-21 12:00:47 +0530

    Add files via upload

---
+++ 4 If Statement.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Expain the if statement
+# Operands:
+#			-eq = if condition is equal value
+#			-ne = if condition is equal or not equal value
+#			-gt = if condition is greater than value
+#			-lt = if condition is less than value
+#			-ge = if condition is greater than or equal value
+#			-le = if condition is less than or equal value
+# if is use start the condition
+# fi is use stop condition
+# Syntax:
+# 		if [condition] 
+# 		then
+#    		statement
+# 		fi
+if [ 5 -eq 5 ]
+than
+	echo "Number is not equal"
+fi
